79f6d24845889471a4ac2b2bda00eb744f61a1dd
Amyliu <1787833697@qq.com>
2025-08-04 12:25:03 +0800

    fix sitemap

--- app/controller/Sitemap.php
+++ app/controller/Sitemap.php
@@ -12,65 +12,86 @@ class Sitemap extends BaseController
 {
     public function index()
     {
-        $baseUrl = Request::domain();
+        $cacheFile = runtime_path() . 'sitemap.xml';
+        $cacheTime = 86400; 
+
+        if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < $cacheTime)) {
+            return Response::create(file_get_contents($cacheFile), 'xml')
+                ->contentType('text/xml; charset=utf-8');
+        }
 
         try {
-            $cates = Db::name('lt_menu')
-                ->distinct(true)
+            $tempFile = tempnam(sys_get_temp_dir(), 'sitemap');
+            $handle = fopen($tempFile, 'w');
+
+            fwrite($handle, '<?xml version="1.0" encoding="UTF-8"?>');
+            fwrite($handle, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+
+            $baseUrl = Request::domain();
+            fwrite($handle, '
+            <url>
+                <loc>' . htmlspecialchars($baseUrl, ENT_QUOTES, 'UTF-8') . '</loc>
+                <lastmod>' . date('Y-m-d') . '</lastmod>
+                <changefreq>daily</changefreq>
+                <priority>1.0</priority>
+            </url>');
+
+            $validCategories = Db::name('lt_menu')
+                ->field('menu_id,menu_title')
                 ->where('menu_website_id', 1)
                 ->order('menu_id desc')
                 ->select();
 
-            $xml = '<?xml version="1.0" encoding="UTF-8"?>
-            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+            foreach ($validCategories as $category) {
+                $safeTitle = htmlspecialchars($category['menu_title'], ENT_QUOTES, 'UTF-8');
+                $categoryUrl = htmlspecialchars($baseUrl, ENT_QUOTES, 'UTF-8') .
+                    '/cate/' . $category['menu_id'] . '/' . $safeTitle . '?page=1';
 
-            $xml .= '
+                fwrite($handle, '
                 <url>
-                    <loc>' . htmlspecialchars($baseUrl, ENT_QUOTES, 'UTF-8') . '</loc>
+                    <loc>' . $categoryUrl . '</loc>
                     <lastmod>' . date('Y-m-d') . '</lastmod>
                     <changefreq>daily</changefreq>
-                    <priority>1.0</priority>
-                </url>';
+                    <priority>0.8</priority>
+                </url>');
 
-            foreach ($cates as $cate) {
-                $safeTitle = htmlspecialchars($cate['menu_title'], ENT_QUOTES, 'UTF-8');
-                $safeBaseUrl = htmlspecialchars($baseUrl, ENT_QUOTES, 'UTF-8');
-                $safeUrl = $safeBaseUrl . '/cate/' . $cate['menu_id'] . '/' . $safeTitle . '?page=1';
-
-                $xml .= '
-                    <url>
-                        <loc>' . $safeUrl . '</loc>
-                        <lastmod>' . date('Y-m-d') . '</lastmod>
-                        <changefreq>daily</changefreq>
-                        <priority>0.8</priority>
-                    </url>';
-
-                $articles = Db::name('lt_content')
-                    ->where('con_mid', $cate['menu_id'])
+                Db::name('lt_content')
+                    ->field('con_id,title')
+                    ->where('con_mid', $category['menu_id'])
                     ->order('con_id desc')
-                    ->select();
+                    ->chunk(500, function ($articles) use ($handle, $baseUrl) {
+                        foreach ($articles as $article) {
+                            $safeTitle = htmlspecialchars($article['title'] ?? '', ENT_QUOTES, 'UTF-8');
+                            $articleUrl = htmlspecialchars($baseUrl, ENT_QUOTES, 'UTF-8') .
+                                '/detail/' . $article['con_id'] . '/' . $safeTitle;
 
-                foreach ($articles as $article) {
-                    $safeArticleTitle = htmlspecialchars($article['title'] ?? '', ENT_QUOTES, 'UTF-8');
-                    $safeArticleUrl = $safeBaseUrl . '/detail/' . $article['con_id'] . '/' . $safeArticleTitle;
-
-                    $xml .= '
-                        <url>
-                            <loc>' . $safeArticleUrl . '</loc>
-                            <lastmod>' . date('Y-m-d') . '</lastmod>
-                            <changefreq>weekly</changefreq>
-                            <priority>0.6</priority>
-                        </url>';
-                }
+                            fwrite($handle, '
+                            <url>
+                                <loc>' . $articleUrl . '</loc>
+                                <lastmod>' . date('Y-m-d') . '</lastmod>
+                                <changefreq>weekly</changefreq>
+                                <priority>0.6</priority>
+                            </url>');
+                        }
+                    });
             }
 
-            $xml .= '
-            </urlset>';
+            fwrite($handle, '</urlset>');
+            fclose($handle);
 
-            return Response::create($xml, 'xml')->contentType('text/xml; charset=utf-8');
+            rename($tempFile, $cacheFile);
+
+            return Response::create(file_get_contents($cacheFile), 'xml')
+                ->contentType('text/xml; charset=utf-8');
         } catch (\Exception $e) {
-            Log::error('Sitemap generation error: ' . $e->getMessage());
-            return Response::create('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>', 'xml');
+            Log::error('Sitemap生成错误: ' . $e->getMessage());
+
+            return Response::create(
+                '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>',
+                'xml',
+                200,
+                ['Content-Type' => 'text/xml; charset=utf-8']
+            );
         }
     }
 }
